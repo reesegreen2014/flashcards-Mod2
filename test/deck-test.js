1a9ee7cdcb04baad942956630698fc369103a3b0
“Reese <reesegreen2014@me.com>
2024-05-09 14:38:31 -0600

    final test suite changes

--- test/deck-test.js
+++ test/deck-test.js
@@ -4,16 +4,16 @@ const expect = chai.expect;
 const { createDeck, createCard } = require('../src/deck');
 
 describe('deck', function() {
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(createDeck).to.be.a('function');
   });
 
-  it.skip('should create an array of the deck of cards', function() {
+  it('should create an array of the deck of cards', function() {
     const deck = createDeck();
     expect(deck).to.be.an('array')
   });  
 
-  it.skip('cards should have properties', function() {
+  it('cards should have properties', function() {
     const deck = createDeck()
     const card1 = deck[0]
     expect(card1).to.have.property('id');
